023328665201cb244d1a64c415845600d3ab056f
nisacdev <nisacolak@outlook.com>
2023-01-03 22:25:42 +0300

    config riverpod

--- lib/router.dart
+++ lib/router.dart
@@ -1,5 +1,6 @@
 import 'package:flutter/material.dart';
 import 'package:flutter_whatsapp_clone/common/widgets/error.dart';
+import 'package:flutter_whatsapp_clone/features/auth/screens/otp_screen.dart';
 
 import 'features/auth/screens/login_screen.dart';
 
@@ -8,6 +9,13 @@ Route<dynamic> generateRoute(RouteSettings settings) {
     case LoginScreen.routeName:
       return MaterialPageRoute(
         builder: (context) => const LoginScreen(),
+      );
+    case OTPScreen.routeName:
+      final verificationId = settings.arguments as String;
+      return MaterialPageRoute(
+        builder: (context) => OTPScreen(
+          verificationId: verificationId,
+        ),
       );
     default:
       return MaterialPageRoute(
