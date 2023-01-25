c48e0bdee1713cfb9e60b0831394c9cabd3ffb3a
nisacdev <nisacolak@outlook.com>
2023-01-25 16:09:48 +0300

    add audio source, record, play etc.

--- lib/features/chat/widgets/bottom_chat_field.dart
+++ lib/features/chat/widgets/bottom_chat_field.dart
@@ -3,8 +3,11 @@ import 'dart:io';
 import 'package:emoji_picker_flutter/emoji_picker_flutter.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter_riverpod/flutter_riverpod.dart';
+import 'package:flutter_sound/public/flutter_sound_recorder.dart';
 import 'package:flutter_whatsapp_clone/common/enums/message_enum.dart';
 import 'package:flutter_whatsapp_clone/features/chat/controller/chat_controller.dart';
+import 'package:path_provider/path_provider.dart';
+import 'package:permission_handler/permission_handler.dart';
 
 import '../../../colors.dart';
 import '../../../common/utils/utils.dart';
@@ -23,8 +26,27 @@ class BottomChatField extends ConsumerStatefulWidget {
 class _BottomChatFieldState extends ConsumerState<BottomChatField> {
   bool isShowSendButton = false;
   final TextEditingController _messageController = TextEditingController();
+  FlutterSoundRecorder? _soundRecorder;
+  bool isRecorderInit = false;
   bool isShowEmojiContainer = false;
+  bool isRecording = false;
   FocusNode focusNode = FocusNode();
+
+  @override
+  void initState() {
+    super.initState();
+    _soundRecorder = FlutterSoundRecorder();
+    openAudio();
+  }
+
+  void openAudio() async {
+    final status = await Permission.microphone.request();
+    if (status != PermissionStatus.granted) {
+      throw RecordingPermissionException('Mic permission not allowed');
+    }
+    await _soundRecorder!.openRecorder();
+    isRecorderInit = true;
+  }
 
   void sendTextMessage() async {
     if (isShowSendButton) {
@@ -32,6 +54,23 @@ class _BottomChatFieldState extends ConsumerState<BottomChatField> {
           context, _messageController.text.trim(), widget.recieverUserId);
       setState(() {
         _messageController.text = '';
+      });
+    } else {
+      var tempDir = await getTemporaryDirectory();
+      var path = '${tempDir.path}/flutter_sound.aac';
+      if (!isRecorderInit) {
+        return;
+      }
+      if (isRecording) {
+        await _soundRecorder!.stopRecorder();
+        sendFileMessage(File(path), MessageEnum.audio);
+      } else {
+        await _soundRecorder!.startRecorder(
+          toFile: path,
+        );
+      }
+      setState(() {
+        isRecording = !isRecording;
       });
     }
   }
@@ -102,6 +141,8 @@ class _BottomChatFieldState extends ConsumerState<BottomChatField> {
   void dispose() {
     super.dispose();
     _messageController.dispose();
+    _soundRecorder!.closeRecorder();
+    isRecorderInit = false;
   }
 
   @override
@@ -197,7 +238,11 @@ class _BottomChatFieldState extends ConsumerState<BottomChatField> {
                 radius: 25,
                 child: GestureDetector(
                   child: Icon(
-                    isShowSendButton ? Icons.send : Icons.mic,
+                    isShowSendButton
+                        ? Icons.send
+                        : isRecording
+                            ? Icons.close
+                            : Icons.mic,
                     color: Colors.white,
                   ),
                   onTap: sendTextMessage,
--- lib/features/chat/widgets/display_text_image_gif.dart
+++ lib/features/chat/widgets/display_text_image_gif.dart
@@ -1,3 +1,4 @@
+import 'package:audioplayers/audioplayers.dart';
 import 'package:cached_network_image/cached_network_image.dart';
 import 'package:flutter/material.dart';
 
@@ -15,7 +16,8 @@ class DisplayTextImageGIF extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    print(message);
+    bool isPlaying = false;
+    final AudioPlayer audioPlayer = AudioPlayer();
     return type == MessageEnum.text
         ? Text(
             message,
@@ -23,14 +25,37 @@ class DisplayTextImageGIF extends StatelessWidget {
               fontSize: 16,
             ),
           )
-        : type == MessageEnum.video
-            ? VideoPlayerItem(
-                videoUrl: message,
-              )
-            : type == MessageEnum.gif
-                ? CachedNetworkImage(
-                    imageUrl: message,
+        : type == MessageEnum.audio
+            ? StatefulBuilder(builder: (context, setState) {
+                return IconButton(
+                  constraints: const BoxConstraints(
+                    minWidth: 100,
+                  ),
+                  onPressed: () async {
+                    if (isPlaying) {
+                      await audioPlayer.pause();
+                      setState(() {
+                        isPlaying = false;
+                      });
+                    } else {
+                      await audioPlayer.play(UrlSource(message));
+                      setState(() {
+                        isPlaying = true;
+                      });
+                    }
+                  },
+                  icon:
+                      Icon(isPlaying ? Icons.pause_circle : Icons.play_circle),
+                );
+              })
+            : type == MessageEnum.video
+                ? VideoPlayerItem(
+                    videoUrl: message,
                   )
-                : CachedNetworkImage(imageUrl: message);
+                : type == MessageEnum.gif
+                    ? CachedNetworkImage(
+                        imageUrl: message,
+                      )
+                    : CachedNetworkImage(imageUrl: message);
   }
 }
